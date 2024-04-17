9022e5d6b945a1e259c4a854f506ab48086bb487
Hobbes0927 <162918563+Hobbes0927@users.noreply.github.com>
2024-04-16 21:32:19 -0600

    Added job lock option

--- client/client.lua
+++ client/client.lua
@@ -1,7 +1,9 @@
 local VORPCore = {}
 
+local jobCheck = true
 local wildHorseRolled = false
 local Spawns = Config.Spawns
+
 
 -- Load VORP Core
 TriggerEvent("getCore", function(core)
@@ -18,10 +20,64 @@ function SetRandomOutfitVar(ped, bool1)
     return Citizen.InvokeNative(0x283978A15512B2FE, ped, bool1)
 end
 
+function DiscordNotification(breed)
+    TriggerServerEvent("hec_wildhorse:discord", breed)
+end
+
+function PlayerTip()
+    VORPCore.NotifyTip(Config.Notification, 4000)
+end
+
+function JobCheck()
+    --Check Job (if applicable)
+    if next(Config.Jobs) ~= nil then
+        local cbresult =  VORPCore.Callback.TriggerAwait('hec_wildhorse:callback:jobcheck', source)
+        if cbresult then
+            return true
+        else
+            return false
+        end
+    else
+        return true
+    end
+end
+
+function SpawnHorse(x, y, z, h, model)
+    --Create Horse
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Citizen.Wait(1)
+    end
+    local wildHorse = CreatePed(model, x, y, z, h, 1, 1)
+    SetEntityInvincible(wildHorse, false)
+    SetRandomOutfitVar(wildHorse, true)
+    SetEntityAsMissionEntity(wildHorse, true, true)
+    SetModelAsNoLongerNeeded(model)
+    
+    while not PedReadyToRender(wildHorse) do 
+        Citizen.Wait(1)
+    end
+
+    Citizen.InvokeNative(0x704C908E9C405136, npc)
+    Citizen.InvokeNative(0xAAB86462966168CE, npc, 1) 
+
+    Citizen.InvokeNative(0x9F7794730795E019, wildHorse, 17, true) -- Set the horse to flee from danger
+    Citizen.InvokeNative(0xAE6004120C18DF97, wildHorse, 0, true) -- -- SET_PED_LASSO_HOGTIE_FLAG 
+    Citizen.InvokeNative(0xAEB97D84CDF3C00B, wildHorse, true) --_SET_ANIMAL_IS_WILD
+
+    SetBlockingOfNonTemporaryEvents(wildHorse, false)  -- Set environment can affect the horse
+
+    TaskWanderStandard(wildHorse, 10.0, 10)  -- Horse behavior is to wander in the direction it's facing
+
+    SetEntityVisible(wildHorse, true, 0)  -- Spawn horse
+end
+
+
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(5000)
-        if not IsEntityDead(PlayerPedId()) then
+        if not IsEntityDead(PlayerPedId()) and jobCheck then
             for k,v in pairs(Spawns) do 
                 for l,c in pairs(v.locations) do        
                     --Check if player has entered one of the spawn zone
@@ -35,46 +91,18 @@ Citizen.CreateThread(function()
                             local horserdm = math.random(1,#v.horses)
                             local horsemodel = v.horses[horserdm]
 
-                            --Create Horse
-                            RequestModel(horsemodel)
-                            while not HasModelLoaded(horsemodel) do
-                                Citizen.Wait(1)
-                            end
                             local heading = math.random(1,359)
                             local x, y, z = table.unpack(c)
-                            local wildHorse = CreatePed(horsemodel, x, y, z, heading, 1, 1)
-                            SetEntityInvincible(wildHorse, false)
-                            SetRandomOutfitVar(wildHorse, true)
-                            SetEntityAsMissionEntity(wildHorse, true, true)
-                            SetModelAsNoLongerNeeded(horsemodel)
-                            
-                            while not PedReadyToRender(wildHorse) do 
-                                Citizen.Wait(1)
-                            end
 
-                            Citizen.InvokeNative(0x704C908E9C405136, npc)
-                            Citizen.InvokeNative(0xAAB86462966168CE, npc, 1) 
-                        
-                            Citizen.InvokeNative(0x9F7794730795E019, wildHorse, 17, true) -- Set the horse to flee from danger
-                            Citizen.InvokeNative(0xAE6004120C18DF97, wildHorse, 0, true) -- -- SET_PED_LASSO_HOGTIE_FLAG 
-                            Citizen.InvokeNative(0xAEB97D84CDF3C00B, wildHorse, true) --_SET_ANIMAL_IS_WILD
-
-                            SetBlockingOfNonTemporaryEvents(wildHorse, false)  -- Set environment can affect the horse
-
-                            TaskWanderStandard(wildHorse, 10.0, 10)  -- Horse behavior is to wander in the direction it's facing
-
-                            SetEntityVisible(wildHorse, true, 0)  -- Spawn horse
-
+                            SpawnHorse(x, y, z, heading, horsemodel)
                             wildHorseRolled = true
 
                             --Discord Notification
-                            if Config.DiscordIntegration then
-                                TriggerServerEvent("hec_wildhorse:discord", v.breed)
-                            end
+                            if Config.DiscordIntegration then DiscordNotification(v.breed) end
 
                             --Player Notification
-                            if Config.NotificationOn then
-                                VORPCore.NotifyTip(Config.Notification, 4000)
+                            if Config.NotificationOn then 
+                                PlayerTip()
                             end
                         end
                     else
@@ -88,6 +116,16 @@ Citizen.CreateThread(function()
                     break
                 end
             end
+        end
+    end
+end)
+
+-- Check for job change every 15 seconds (if applicable)
+Citizen.CreateThread(function()
+    while true do
+        if not IsEntityDead(PlayerPedId()) then
+            jobCheck = JobCheck()
+            Citizen.Wait(15000)
         end
     end
 end)
